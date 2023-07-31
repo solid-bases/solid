7dcb02cf06b7bd361e3d78ad0909710e41e4b927
Fabio Cocchi <fabio.cocchi90@gmail.com>
2023-07-31 11:45:47 +0200

    fix(WallConfiguration): greyPattern now is not passed in the constructor

--- Bricklayer/Builder/GeneralWallConfiguration.cs
+++ Bricklayer/Builder/GeneralWallConfiguration.cs
@@ -4,33 +4,7 @@ internal class GeneralWallConfiguration
 {
     private readonly int _totalWidth = 180;
     private readonly int _totalHeight = 90;
-    protected GreyPattern _greyPattern;
-
-    public GeneralWallConfiguration()
-    {
-        _greyPattern = new GreyPattern(new[] {
-            new RowPattern {
-                RowNumber = 3,
-                ColumnsNumber = new[] { 5 }
-            },
-            new RowPattern {
-                RowNumber = 4,
-                ColumnsNumber = new[] { 5,6 }
-            },
-            new RowPattern {
-                RowNumber = 5,
-                ColumnsNumber = new[] { 4,6 }
-            },
-            new RowPattern {
-                RowNumber = 6,
-                ColumnsNumber = new[] { 5,6 }
-            },
-            new RowPattern {
-                RowNumber = 7,
-                ColumnsNumber = new[] { 5 }
-            },
-        });
-    }
+    protected GreyPattern _greyPattern = new(Array.Empty<RowPattern>());
 
     public RowBricks[] BuildWall() => new BuildWall(_totalHeight, _totalWidth, _greyPattern).ToArray();
 }
--- Bricklayer/Builder/StandardWallBuilder.cs
+++ Bricklayer/Builder/StandardWallBuilder.cs
@@ -2,8 +2,29 @@ namespace Bricklayer.Builder;
 
 internal class StandardWallConfiguration : GeneralWallConfiguration
 {
-    public StandardWallConfiguration(GreyPattern greyPattern)
+    public StandardWallConfiguration()
     {
-        _greyPattern = greyPattern;
+        _greyPattern = new GreyPattern(new[] {
+            new RowPattern {
+                RowNumber = 3,
+                ColumnsNumber = new[] { 5 }
+            },
+            new RowPattern {
+                RowNumber = 4,
+                ColumnsNumber = new[] { 5,6 }
+            },
+            new RowPattern {
+                RowNumber = 5,
+                ColumnsNumber = new[] { 4,6 }
+            },
+            new RowPattern {
+                RowNumber = 6,
+                ColumnsNumber = new[] { 5,6 }
+            },
+            new RowPattern {
+                RowNumber = 7,
+                ColumnsNumber = new[] { 5 }
+            },
+        });
     }
 }
